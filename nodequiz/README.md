49df868c1b9ad384b7aec49098236f9861ee7b64
ethan townsend <eetfitness@gmail.com>
2019-09-18 17:23:22 -0700

    edit

--- nodequiz/README.md
+++ nodequiz/README.md
@@ -1,6 +1,6 @@
 # Nodequiz
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.1.2.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 8.3.5.
 
 ## Development server
 
--- nodequiz/e2e/src/app.e2e-spec.ts
+++ nodequiz/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to nodequiz!');
+    expect(page.getTitleText()).toEqual('nodequiz app is running!');
   });
 
   afterEach(async () => {
