9a165f131c05952c1bd5df5bfca645e2ca73330e
ethan townsend <eetfitness@gmail.com>
2019-09-28 23:36:35 -0700

    edit

--- nodequizz/server/models/users.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const userSchema = mongoose.Schema({
-    id: { type: String, required: true},
-    firstName: { type: String, required: false },
-    lastName: { type: String, required: false },
-});
-
-module.exports = mongoose.model('User', userSchema);
