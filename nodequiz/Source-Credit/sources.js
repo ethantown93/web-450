5ec4844efbef6b8044992a84029c5c6aef4b8feb
ethan townsend <eetfitness@gmail.com>
2019-09-29 20:38:24 -0700

    adding sources

---
+++ nodequiz/Source-Credit/sources.js
@@ -0,0 +1,5 @@
+// Source for code credit: https://www.youtube.com/watch?v=ozXGkqpzo_A&list=PLC3y8-rFHvwg2RBz6UplKTGIXREj9dV0G
+
+// many of my pages contain code/partial code from this video series, so I figured
+// it would be better to create a folder dedicated to the source instead of citing it in 
+// every file.
