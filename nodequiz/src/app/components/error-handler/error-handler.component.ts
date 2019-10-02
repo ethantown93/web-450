93a5d96eb6cae46397afc0eeb4e56e6abe75024d
ethan townsend <eetfitness@gmail.com>
2019-10-01 23:42:26 -0700

    adding error handling

---
+++ nodequiz/src/app/components/error-handler/error-handler.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Inject } from '@angular/core';
+import { MAT_DIALOG_DATA } from '@angular/material';
+
+@Component({
+  selector: 'app-error-handler',
+  templateUrl: './error-handler.component.html',
+  styleUrls: ['./error-handler.component.css']
+})
+export class ErrorHandlerComponent implements OnInit {
+
+  message = 'A gosh darn error has done occured ayyyut';
+
+  constructor(@Inject(MAT_DIALOG_DATA) public data: {message: string}) { }
+
+  ngOnInit() {
+  }
+
+}
